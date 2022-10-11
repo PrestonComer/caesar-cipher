373f222f34a46604d148e55a3042b416752cb372
PrestonComer <1123preston1123@gmail.com>
2022-10-11 12:46:02 -0500

    trying another way to do the cipher

--- caesar-cipher.cpp
+++ caesar-cipher.cpp
@@ -4,9 +4,7 @@
 #include <algorithm>
 
 int main() {
-    std::string userString, userShift, newString, token;
-    int k, newStringAscii, pos, loc = 0;
-
+    std::string userString;
     std::cout << "Enter String To Be Encrypted: ";
     std::cin >> userString;
 
@@ -16,7 +14,19 @@ int main() {
         std::cin >> userString;
     }
 
+    // get from the user if we should encrypt or decrypt
+    std::string process;
+    std::cout << "Do You Want To Encrypt Or Decrypt A Message? (E/D): ";
+    std::cin >> process;
+
+    // make sure that the given direction is valid
+    while(!process.find_first_not_of("eEdD")) {
+        std::cout << "Improper Input. Please Try Again: ";
+        std::cin >> process;
+    }
+
     // get the values to shift the given string
+    std::string userShift;
     std::cout << "Enter The Desired Shift Value Deliminated With Spaces. All Missing Values Default To 1: ";
     std::cin >> userShift;
 
@@ -26,26 +36,63 @@ int main() {
         std::cin >> userShift;
     }
 
-
+    // if there is only a single value to shift then set entire vector to that value
     std::vector<std::string> userShiftVector(userString.length(), "1");
-
     if (userShift.size() == 1) {
-        userShiftVector.at(0) = userShift;
+        for (int i = 0; i < userShiftVector.size(); i++) {
+            userShiftVector.at(i) = userShift;
+        }
     }
 
     // take the shift list apart
+    int pos, loc = 0;
     while ((pos = userShift.find(" ")) != std::string::npos) {
         userShiftVector.at(loc) = userShift.substr(0, pos);
         userShift.erase(0, pos + 1);
+        loc++;
     }
 
-    for (int i = 0; i < userShiftVector.size(); i++) {
-        newString += userString[i] + std::stoi(userShiftVector.at(i));
+    std::string newString;
+    bool encryptMessage;
+    std::transform(process.begin(), process.end(), process.begin(), ::toupper);
+    if (process == "E") {
+        encryptMessage = true;
+    } else {
+        encryptMessage = false;
     }
 
-    std::cout << "Original String: " << userString << std::endl 
-              << "Shift Value: " << userShift << std::endl 
-              << "Resulting String: " << newString << std::endl;
+    switch (encryptMessage) {
+        case 1: // encrypt message
+            int newCharAscii;
+            char newChar;
+            
+            for (int i = 0; i < userString.length(); i++) {
+                newCharAscii = userString[i] + std::stoi(userShiftVector.at(i));
+                newChar = char(newCharAscii);
+                
+                if (userString[i] >= 'a' and userString[i] <= 'z' and newChar > 'z') {
+                        newCharAscii = newCharAscii - 'z' + 'a' - 1;
+                        newChar = char(newCharAscii);
+                } else if (userString[i] >= 'A' and userString[i] <= 'Z' and newChar > 'Z') {
+                        newCharAscii = newCharAscii - 'Z' + 'A' - 1;
+                        newChar = char(newCharAscii);
+                }
+                std::cout << userString[i] << " : " << userString[i] + 0 << " => " << newChar << " : " << newCharAscii << std::endl;
+            }
+
+            std::cout << "Original String: " << userString << std::endl 
+                      << "Shift Value: " << userShift << std::endl 
+                      << "Resulting String: " << newString << std::endl;
+            break;
+        default: // decrypt message
+            std::transform(userString.begin(), userString.end(), userString.begin(), ::tolower);
+            for (int i = 1; i < 26; i++) {
+                for (int j = 0; j < newString.size(); j++) {
+                    break;
+                }
+            }
+            break;
+    }
 
     // keep exe open
     char c; std::cin >> c;
